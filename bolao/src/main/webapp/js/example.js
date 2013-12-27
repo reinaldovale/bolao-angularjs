4b879dfd901fc8d506799cf3e0e481e0f52860cc
reinaldovale <reinaldovale@gmail.com>
2013-12-27 21:38:22 -0200

    Experimento com ngAnimate

--- bolao/src/main/webapp/js/example.js
+++ bolao/src/main/webapp/js/example.js
@@ -1,9 +1,26 @@
-angular.module('bolao', ['ui.bootstrap']);
-function ClassificacaoCtrl($scope) {
+var MyApp = angular.module("bolao", ['ui.bootstrap', 'ngAnimate', 'ui.bootstrap']);
+
+MyApp
+.directive('setNgAnimate', ['$animate', function ($animate) {
+ return {
+     link: function ($scope, $element, $attrs) { 
+       
+         $scope.$watch( function() { 
+                 return $scope.$eval($attrs.setNgAnimate, $scope); 
+             }, function(valnew, valold){
+                 $animate.enabled(!!valnew, $element);
+         });  
+         
+         
+     }
+ };
+}])
+.controller('ClassificacaoCtrl',['$scope', '$animate', function ($scope, $animate) {    
+    
     $scope.oneAtATime = true;
     
     $scope.boleiros = [{
-            nome: "Reinaldo Valee",
+            nome: "Reinaldo Vale",
             img: "img/daniel.png",
             pontos: 100,
             placares: 50
@@ -24,31 +41,39 @@ function ClassificacaoCtrl($scope) {
             placares: 4
         }];
 
-}
+}]).
+controller('RodadaCtrl',['$scope', '$animate', function ($scope, $animate) {   
 
-function RodadaCtrl($scope) {
+     
+    $scope.carouselCaption = true;
     
-  $scope.carouselCaption = true;
+    $scope.myInterval = 5000;
+    var rodadas = $scope.rodadas = [];
+    $scope.addSlide = function(numero) {
+        var newWidth = 200 + ((rodadas.length + (25 * rodadas.length)) % 150);
+        rodadas.push({numero: numero,
+            data: '25/09/2013',
+            jogos: [
+                {mandante: 'Fla',convidado: 'Bot'}, 
+                {mandante: 'Vas',convidado: 'Par'}, 
+                {mandante: 'Fla',convidado: 'Bot'}, 
+                {mandante: 'Vas',convidado: 'Par'}, 
+                {mandante: 'Fla',convidado: 'Bot'}, 
+                {mandante: 'Vas',convidado: 'Par'}, 
+                {mandante: 'Fla',convidado: 'Bot'}, 
+                {mandante: 'Vas',convidado: 'Par'}, 
+                {mandante: 'Fla',convidado: 'Bot'}, 
+                {mandante: 'Vas',convidado: 'Par'}]});
+    };
+    for (var i = 0; i < 10; i++) {
+        $scope.addSlide(i);
+    }
     
-  $scope.myInterval = 5000;
-  var rodadas = $scope.rodadas = [];
-  $scope.addSlide = function(numero) {
-    var newWidth = 200 + ((rodadas.length + (25 * rodadas.length)) % 150);
-    rodadas.push({numero: numero,
-                  data: '25/09/2013', 
-                  jogos: [
-                    {mandante:'Fla', convidado : 'Bot'}, 
-                    {mandante:'Vas', convidado : 'Par'}, 
-                    {mandante:'Fla', convidado : 'Bot'}, 
-                    {mandante:'Vas', convidado : 'Par'}, 
-                    {mandante:'Fla', convidado : 'Bot'}, 
-                    {mandante:'Vas', convidado : 'Par'}, 
-                    {mandante:'Fla', convidado : 'Bot'}, 
-                    {mandante:'Vas', convidado : 'Par'}, 
-                    {mandante:'Fla', convidado : 'Bot'}, 
-                    {mandante:'Vas', convidado : 'Par'}]});
-  };
-  for (var i=0; i<10; i++) {
-    $scope.addSlide(i);
-  }
-}
+    $scope.animate = true;
+    $scope.animateGlobal = false;
+    
+    $scope.$watch('animateGlobal', function(val){
+        console.log('Set Global animation Enabled: ' + val);
+        $animate.enabled(val); 
+    });
+}]);
