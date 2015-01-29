cb6c635345cd89d062be6a6b9b8b3bb37d67bb27
reinaldovale <reinaldovale@gmail.com>
2015-01-29 05:23:18 -0200

    refactory do código e exclusão do módulo de animação

--- bolao/src/main/webapp/ui/carrossel/js/carrossel.js
+++ bolao/src/main/webapp/ui/carrossel/js/carrossel.js
@@ -1,75 +1,49 @@
 'use strict';
 
-angular.module('bolao.diretivas.carrossel', ['ngAnimate'])
+// angular.module('bolao.carrossel', ['ngAnimate'])
+angular.module('bolao.carrossel', [])
     .directive('carrossel', function() {
         return {
             restrict: 'E',
             transclude: true,
-            replace: true,
+//             replace: true,
             templateUrl: 'ui/carrossel/diretivas/diretiva-carrossel.html' ,
-            scope: false,
+            scope: true,
             controller: function ($scope) {
-                var eu = this,
-                slides = $scope.slides = [];                
-                        
-                eu.direction = $scope.direction = 'right';
-                eu.mostrarDetalhe = true;
-                $scope.currentIndex = 0;
+              
+                var eu = this;
+                $scope.paginas = [];
+                $scope.indiceAtual = 0;
                 
-                eu.adicionarPagina = function(pagina, element) {
-                  slides.push(pagina);                  
+                eu.adicionarPagina = function(pagina) {
+                  if($scope.paginas.length === 0) {
+                    pagina.exibir("esquerda");
+                  }
+                  $scope.paginas.push(pagina);                  
                 };
         
-                $scope.setCurrentSlideIndex = eu.setCurrentSlideIndex = function (index) {
-                    index = parseInt(index);        		
-                    if(typeof index === 'number' && isFinite(index) && index > -1 && index < $scope.slides.length) {
-                      if($scope.currentIndex < index) {
-                        $scope.currentIndex = index - 1;
-                        $scope.nextSlide();
-                      }
-                      else {
-                        $scope.currentIndex = index + 1;
-                        $scope.prevSlide();
-                      }
-                      
-                            
-//                          $scope.direction = eu.direction = (index > $scope.currentIndex) ? 'left' : 'right';
-//                         $scope.currentIndex = index;
-                    }
-                    
+                $scope.atualizarIndice = function (indice, direcao) {
+                  $scope.paginas[$scope.indiceAtual].ocultar();                  
+                  $scope.paginas[indice].exibir(direcao);
+                  $scope.indiceAtual = indice;
                 };
         
-                $scope.isCurrentSlideIndex = function (index) {
-                    return eu.isCurrentSlideIndex(index);
-                };
-                
-                eu.isCurrentSlideIndex = function (index) {
-                    return $scope.currentIndex === index;
+                $scope.ehIndicePaginaAtual = function (indice) {                    
+                    return $scope.indiceAtual === indice;
                 };
         
-                $scope.nextSlide = function () {
-                     $scope.direction = eu.direction = 'right';
-//                     angular.forEach(slides, function(slide) {
-//                         slide.direction = $scope.direction;
-//                     });
-                    $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
-                    slides.forEach(function(slide) {
-                      slide.direction = 'right';
-                    });
+                $scope.proximaPagina = function () {  
+                  var indiceAtual = $scope.indiceAtual,           
+                  novoIndiceAtual = (indiceAtual < $scope.paginas.length - 1) ? ++indiceAtual : 0;
+                    $scope.atualizarIndice(novoIndiceAtual, "direita");
                 };
         
-                $scope.prevSlide = function () {
-                    $scope.direction = eu.direction = 'left';
-//                     angular.forEach(slides, function(slide) {
-//                         slide.direction = $scope.direction;
-//                     });
-                    $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
-                    slides.forEach(function(slide) {
-                      slide.direction = 'left';
-                    });
+                $scope.paginaAnterior = function () {
+                    var indiceAtual = $scope.indiceAtual,
+                    novoIndiceAtual = (indiceAtual > 0) ? --indiceAtual : $scope.paginas.length - 1;                    
+                    $scope.atualizarIndice(novoIndiceAtual, "esquerda");
                 };
-            }
-            
+            }            
         };
     })
     .directive('pagina', function() {
@@ -79,25 +53,28 @@ angular.module('bolao.diretivas.carrossel', ['ngAnimate'])
       transclude: true,
 //       replace: true,
       scope: {
-        mostrarDescricao: '@',
+        conteudo: '='
       },
       link: function(scope, element, attrs, carrosselControle) {
-        scope.isCurrentSlideIndex = function () {
-          return carrosselControle.isCurrentSlideIndex(scope.$parent.$index);
-        };
         
-        scope.setCurrentSlideIndex = function () {
-          carrosselControle.setCurrentSlideIndex(scope.$parent.$index);
+        scope.exibir = function(direcao) {          
+            scope.visivel = true;
+            scope.direcao = direcao;
+            scope.detalheVisivel = true;
+        }
+
+        scope.ocultar = function() {          
+            scope.visivel = false;
+            scope.detalheVisivel = false;
+        }
+                                  
+        scope.alternarDetalhe = function() {          
+            scope.detalheVisivel = !scope.detalheVisivel;
+            return scope.detalheVisivel;
         };
-        
-        carrosselControle.adicionarPagina(scope, element);
-        
-        scope.detalheVisivel = true;
-                                      
-        scope.alternarDetalhe = function(){
-            return scope.isCurrentSlideIndex() && scope.detalheVisivel;
-//             scope.detalheVisivel = !scope.detalheVisivel;
-        };
+
+        scope.ocultar();
+        carrosselControle.adicionarPagina(scope);
       },
       templateUrl: 'ui/carrossel/diretivas/diretiva-paginas.html'
     };
