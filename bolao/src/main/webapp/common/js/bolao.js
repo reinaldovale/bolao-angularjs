61d4f18049123f8291524d07cc40f16a3da3909b
Reinaldo Bezerra do Vale <reinaldo.vale@serpro.gov.br>
2015-02-05 19:39:05 -0200

    Adição da funcionalidade de acordeon.

--- bolao/src/main/webapp/common/js/bolao.js
+++ bolao/src/main/webapp/common/js/bolao.js
@@ -1,4 +1,4 @@
-var bolao = angular.module('bolao', ['bolao.carrossel']);
+var bolao = angular.module('bolao', ['bolao.carrossel', 'bolao.acordeon']);
 
 bolao.factory('locations', function($http) {
   var promise = null;
@@ -16,12 +16,49 @@ bolao.factory('locations', function($http) {
 });
 
 bolao.controller('RodadaControle', ['$scope', function($scope) {
-    $scope.rodadas = [
+  $scope.boleiros = [
+                    {nome: 'Reinaldo Bezerra do Vale', pontos: 63, placares: 40, rodadas : [
                     {image: 'common/img/pages/img00.jpg', descricao: 'Image 00', detalhe: true},
                     {image: 'common/img/pages/img01.jpg', descricao: 'Image 01', detalhe: false},
                     {image: 'common/img/pages/img02.jpg', descricao: 'Image 02', detalhe: false},
                     {image: 'common/img/pages/img03.jpg', descricao: 'Image 03', detalhe: true},
                     {image: 'common/img/pages/img04.jpg', descricao: 'Image 04', detalhe: false}
+                ]},
+                    {nome: 'Rosiel', pontos: 00000, placares: 40, rodadas : [
+                    {image: 'common/img/pages/img00.jpg', descricao: 'Image 00', detalhe: true},
+                    {image: 'common/img/pages/img01.jpg', descricao: 'Image 01', detalhe: false},
+                    {image: 'common/img/pages/img02.jpg', descricao: 'Image 02', detalhe: false},
+                    {image: 'common/img/pages/img03.jpg', descricao: 'Image 03', detalhe: true},
+                    {image: 'common/img/pages/img04.jpg', descricao: 'Image 04', detalhe: false}
+                ]},
+                    {nome: 'Corró', pontos: 11111, placares: 40, rodadas : [
+                    {image: 'common/img/pages/img00.jpg', descricao: 'Image 00', detalhe: true},
+                    {image: 'common/img/pages/img01.jpg', descricao: 'Image 01', detalhe: false},
+                    {image: 'common/img/pages/img02.jpg', descricao: 'Image 02', detalhe: false},
+                    {image: 'common/img/pages/img03.jpg', descricao: 'Image 03', detalhe: true},
+                    {image: 'common/img/pages/img04.jpg', descricao: 'Image 04', detalhe: false}
+                ]},
+                    {nome: 'Rosinete', pontos: 2222, placares: 40, rodadas : [
+                    {image: 'common/img/pages/img00.jpg', descricao: 'Image 00', detalhe: true},
+                    {image: 'common/img/pages/img01.jpg', descricao: 'Image 01', detalhe: false},
+                    {image: 'common/img/pages/img02.jpg', descricao: 'Image 02', detalhe: false},
+                    {image: 'common/img/pages/img03.jpg', descricao: 'Image 03', detalhe: true},
+                    {image: 'common/img/pages/img04.jpg', descricao: 'Image 04', detalhe: false}
+                ]},
+                    {nome: 'Sitonho', pontos: 3333, placares: 40, rodadas : [
+                    {image: 'common/img/pages/img00.jpg', descricao: 'Image 00', detalhe: true},
+                    {image: 'common/img/pages/img01.jpg', descricao: 'Image 01', detalhe: false},
+                    {image: 'common/img/pages/img02.jpg', descricao: 'Image 02', detalhe: false},
+                    {image: 'common/img/pages/img03.jpg', descricao: 'Image 03', detalhe: true},
+                    {image: 'common/img/pages/img04.jpg', descricao: 'Image 04', detalhe: false}
+                ]},
+                    {nome: 'Neneco', pontos: 4444, placares: 40, rodadas : [
+                    {image: 'common/img/pages/img00.jpg', descricao: 'Image 00', detalhe: true},
+                    {image: 'common/img/pages/img01.jpg', descricao: 'Image 01', detalhe: false},
+                    {image: 'common/img/pages/img02.jpg', descricao: 'Image 02', detalhe: false},
+                    {image: 'common/img/pages/img03.jpg', descricao: 'Image 03', detalhe: true},
+                    {image: 'common/img/pages/img04.jpg', descricao: 'Image 04', detalhe: false}
+                ]}
                 ];
 //     var t = true;                
 //     $scope.alternar = function() {
