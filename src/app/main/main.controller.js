99fb82c0dd4c1969628af851acf2c66532362352
Reinaldo Bezerra do Vale <reinaldo.vale@serpro.gov.br>
2015-04-09 19:11:29 -0300

    json gerado automaticamente completo com fotos e escudos dos times

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,19 +1,12 @@
 'use strict';
 
 angular.module('bolao')
-  .controller('ControlePrincipal', ['$scope', 'BD', function($scope, BD) {
-
-    $scope.boleiros = [];
-
-//     var bd = new BD();
-//     bd.pegarBoleiros().then(function() {
-//         $scope.boleiros = bd.boleiros;
-//     });
-
-    
-    BD.pegarBoleiros()
-      .then(function(dados) {
-            $scope.boleiros = dados;            
-       });
-    
-}]);
+.controller('ControlePrincipal', ['$scope', 'BD', function($scope, BD) {
+        
+        $scope.boleiros = [];
+        
+        BD.pegarBoleiros()
+        .then(function(dados) {
+            $scope.boleiros = dados;
+        });
+    }]);
