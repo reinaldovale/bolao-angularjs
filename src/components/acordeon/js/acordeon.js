b6e971202c7a25b34d9e758f56510bffac9155e5
Reinaldo Bezerra do Vale <reinaldo.vale@serpro.gov.br>
2015-07-24 18:40:36 -0300

    Destacar rebaixados incompleto

--- src/components/acordeon/js/acordeon.js
+++ src/components/acordeon/js/acordeon.js
@@ -12,50 +12,108 @@ angular.module('bolao.acordeon', [])
             $scope.itens = [];
             
             var eu = this,
-            maxPremiados = 2,
+            TOTAL_PREMIADOS = 2,
             minPontoPremiado = 0,
+            minPontoRebaixado = 0,
+            minPlacarRebaixado = 0,
             premiados = 0,
-            maxRebaixado = 2,
+            TOTAL_REBAIXADOS = 2,
             rebaixados = 0,
             indiceAtual = 0,
+            
             verificarPremiado = function(itemCorrente) {                
-                if(premiados < maxPremiados) {
-                    itemCorrente.tipo = "item-premiado";
-                    premiados++;     
-                    if(minPontoPremiado === 0) {
+                if(premiados < TOTAL_PREMIADOS && (minPontoPremiado === 0 || minPontoPremiado >= itemCorrente.boleiro.pontos)) {
+                        itemCorrente.tipo = 'item-premiado';
+                        premiados++;
                         minPontoPremiado = itemCorrente.boleiro.pontos;
-                    }
-                    if(minPontoPremiado > itemCorrente.boleiro.pontos) {
-                        minPontoPremiado = itemCorrente.boleiro.pontos;
-                    }
                 }
                 else {
                     if(minPontoPremiado < itemCorrente.boleiro.pontos) {
-                        angular.forEach($scope.itens, function(item, indice) {
+                        angular.forEach($scope.itens, function(item) {
                                 if(item.boleiro.pontos === minPontoPremiado) {
-                                    item.tipo = "";
+                                    item.tipo = '';
                                 }
                         });
                         minPontoPremiado = itemCorrente.boleiro.pontos;  
-                        itemCorrente.tipo = "item-premiado";                
+                        itemCorrente.tipo = 'item-premiado';                
                     }
                     else {
                         if(minPontoPremiado === itemCorrente.boleiro.pontos) {
-                            angular.forEach($scope.itens, function(item, indice) {
+                            angular.forEach($scope.itens, function(item) {
                                 if(item.boleiro.pontos === minPontoPremiado) {
                                     if(itemCorrente.boleiro.placares > item.boleiro.placares) {
-                                        item.tipo = "";
-                                        itemCorrente.tipo = "item-premiado";
+                                        item.tipo = '';
+                                        itemCorrente.tipo = 'item-premiado';
                                     }
                                     else if(itemCorrente.boleiro.placares === item.boleiro.placares) {
-                                        itemCorrente.tipo = "item-premiado";
+                                        itemCorrente.tipo = 'item-premiado';
                                     }
                                 }
                             });
                         }
+                        else {
+                            itemCorrente.tipo = '';
+                            verificarRebaixado(itemCorrente);
+                        }
                     }                    
                 }           
-            };            
+            },    
+
+            verificarRebaixado = function(itemCorrente) {
+console.log('Entrada: \tPT = \t' + itemCorrente.boleiro.pontos + '\tPL = \t' + itemCorrente.boleiro.placares + '\tTipo = ' + itemCorrente.tipo + '\nminPT = ' + minPontoRebaixado +  '\tminPL = ' + minPlacarRebaixado + '\tRebaixados = ' + rebaixados);                                        
+                if(rebaixados < TOTAL_REBAIXADOS) {
+//console.log('PT = \t' + itemCorrente.boleiro.pontos + '\tPL = \t' + itemCorrente.boleiro.placares + '\tTipo = ' + itemCorrente.tipo + '\nminPT = ' + minPontoRebaixado +  '\tminPL = ' + minPlacarRebaixado + '\tRebaixados = ' + rebaixados);                        
+                    itemCorrente.tipo = 'item-rebaixado';
+                    rebaixados++;                   
+                    if(minPontoRebaixado === 0) { 
+                        minPontoRebaixado = itemCorrente.boleiro.pontos;
+                        minPlacarRebaixado = itemCorrente.boleiro.placares;
+                    }
+                    else if(minPontoRebaixado < itemCorrente.boleiro.pontos) {
+                                minPontoRebaixado = itemCorrente.boleiro.pontos;
+                                minPlacarRebaixado = itemCorrente.boleiro.placares;
+                        }
+                    else if(minPontoRebaixado === itemCorrente.boleiro.pontos) {
+                        if(minPlacarRebaixado < itemCorrente.boleiro.placares) {
+                                minPlacarRebaixado = itemCorrente.boleiro.placares;
+                        }
+                        if(minPlacarRebaixado === itemCorrente.boleiro.placares) {
+                                rebaixados--;
+                        }                                               
+                    }
+                }
+                 else if(minPontoRebaixado >= itemCorrente.boleiro.pontos) {  
+//console.log('PT = \t' + itemCorrente.boleiro.pontos + '\tPL = \t' + itemCorrente.boleiro.placares + '\tTipo = ' + itemCorrente.tipo + '\nminPT = ' + minPontoRebaixado +  '\tminPL = ' + minPlacarRebaixado + '\tRebaixados = ' + rebaixados);                    
+                        var rebaixadoSaiu = false,
+                            minPontoRebaixadoTemp = itemCorrente.boleiro.pontos,
+                            minPlacarRebaixadoTemp = itemCorrente.boleiro.placares;
+                            
+                        angular.forEach($scope.itens, function(item) {
+                                if(item.tipo ==='item-rebaixado') {
+                                        if(item.boleiro.pontos === minPontoRebaixado) {
+                                                if(item.boleiro.pontos > itemCorrente.boleiro.pontos || (item.boleiro.pontos === itemCorrente.boleiro.pontos && item.boleiro.placares > itemCorrente.boleiro.placares) ) {
+                                                        item.tipo = '';
+                                                        rebaixadoSaiu = true;
+                                                        itemCorrente.tipo = 'item-rebaixado';
+                                                }                                                                                                     
+                                        }
+                                        if(item.tipo ==='item-rebaixado') {
+                                                                                                
+                                                if(item.boleiro.pontos > minPontoRebaixadoTemp || (item.boleiro.pontos === minPontoRebaixadoTemp && item.boleiro.placares > minPlacarRebaixadoTemp)) {
+                                                        minPontoRebaixadoTemp = item.boleiro.pontos;
+                                                        minPlacarRebaixadoTemp = item.boleiro.placares;
+                                                }
+                                        }
+                                }
+                        });
+                        if(rebaixadoSaiu) {
+                                rebaixados--;
+                        }
+                        minPontoRebaixado = minPontoRebaixadoTemp;
+                        minPlacarRebaixado = minPlacarRebaixadoTemp;            
+                }
+console.log('Saída: \tPT = \t' + itemCorrente.boleiro.pontos + '\tPL = \t' + itemCorrente.boleiro.placares + '\tTipo = ' + itemCorrente.tipo + '\nminPT = ' + minPontoRebaixado +  '\tminPL = ' + minPlacarRebaixado + '\tRebaixados = ' + rebaixados);                                      
+            };        
             
             eu.adicionarItem = function(item) {
                 item.tipo = 'premiado';
