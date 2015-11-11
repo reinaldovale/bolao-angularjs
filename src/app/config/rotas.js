9e938773136d120291d43bbd1d9f0dcec33d3373
Reinaldo Bezerra do Vale <reinaldo.vale@serpro.gov.br>
2015-11-11 00:45:46 -0200

    ajustes na pasta dos htmls parciais

--- src/app/config/rotas.js
+++ src/app/config/rotas.js
@@ -1,11 +1,11 @@
 angular.module("bolao")
 .config(function ($routeProvider) {
 	$routeProvider.when("/", {
-		templateUrl: "parcial/inicio.html",
+		templateUrl: "app/parcial/inicio.html",
 		controller: "ControlePrincipal"	
 	});
 	$routeProvider.when("/adm", {
-		templateUrl: "partial/teste.html",
+		templateUrl: "app/parcial/teste.html",
 		controller: "ControleAtualizacao"
 	});	
 	$routeProvider.otherwise({redirectTo: "/"});
