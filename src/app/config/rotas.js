b99cf147bf32e570a95aa88d521475f4ceb17153
Reinaldo Bezerra do Vale <reinaldo.vale@serpro.gov.br>
2016-05-04 00:00:09 -0300

    primeira versão do card

--- src/app/config/rotas.js
+++ src/app/config/rotas.js
@@ -22,4 +22,26 @@ angular.module("bolao")
 	$routeProvider.otherwise({redirectTo: "/"});
 	$locationProvider.html5Mode(true);
 // 	$httpProvider.useApplyAsync(true);
+})
+.run(function () {
+    var mdlUpgradeDom = false;
+    setInterval(function() {
+      if (mdlUpgradeDom) {
+        componentHandler.upgradeDom();
+        mdlUpgradeDom = false;
+      }
+    }, 200);
+
+    var observer = new MutationObserver(function () {
+      mdlUpgradeDom = true;
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+    /* support <= IE 10
+    angular.element(document).bind('DOMNodeInserted', function(e) {
+        mdlUpgradeDom = true;
+    });
+    */
 });
